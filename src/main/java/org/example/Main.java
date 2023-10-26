481a16c1cca98c2852f2993202dc113be268084c
Vladimir <70057256+samthezpub@users.noreply.github.com>
2023-10-26 20:37:44 +0300

    Notes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> list = List.of("Привет!", "Я", "Олег");
+
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+        // Пример как можно удалить всё с помощью итератора
+        List<String> list1NotImmutable = new ArrayList<>();
+        list1NotImmutable.add("Привет!");
+        list1NotImmutable.add("Я");
+        list1NotImmutable.add("Олег!");
+
+
+        Iterator<String> iterator2 = list1NotImmutable.iterator();
+        while (iterator2.hasNext()){
+            iterator2.next();
+            iterator2.remove();
+        }
+        System.out.println(list1NotImmutable);
     }
 }
